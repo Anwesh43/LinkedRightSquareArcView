92c52f8ba00e893199b50ecc536a5826cf7e682d
Anwesh43 <anweshthecool0@gmail.com>
2021-04-14 01:44:10 +0530

    adding fullscreen support

--- app/src/main/java/com/example/linkedrightsquarearcview/MainActivity.kt
+++ app/src/main/java/com/example/linkedrightsquarearcview/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.linkedrightsquarearcview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.example.rightsquarearcview.RightSquareArcView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        RightSquareArcView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
